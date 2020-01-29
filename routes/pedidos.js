2b64995e5065d2c4e6d4a9854a8dd07ef591cf72
Klowty <kinggbr0102@gmail.com>
2020-01-29 15:19:53 -0300

    Rotas

---
+++ routes/pedidos.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+
+//RETORNA TODOS OS PEDIDOS
+router.get('/', (req, res, next) => {
+    res.status(200).send({
+        mensagem: 'Retorna os pedidos'
+    });
+});
+
+//INSERE UM PEDIDO
+router.post('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: 'O pedido foi criado'
+    });
+});
+
+
+//RETORNA UM PEDIDO ESPECÍFICO
+router.get('/:id_pedido', (req, res, next) => {
+    const id = req.params.id_pedido; //pega da url
+        res.status(200).send({
+            mensagem: 'Detalhes do pedido',
+            id_pedido: id
+        });
+});
+
+//EXCLUI UM PEDIDO
+router.delete('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: 'Pedido excluído'
+    });
+});
+
+module.exports = router;
